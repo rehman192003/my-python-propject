17f5a629b007c2b8baea2a022a1af1d5e8dcba4f
Your Name <rana.cht.45@gmail.com>
2025-10-15 09:19:53 -0700

    changing the code

--- for_loop.py
+++ for_loop.py
@@ -1,14 +1,14 @@
-# colors={'red','green','yellow'}
-# for color in colors:
-#     print(color)
-#     for i in color:
-#         if i=='r':
-#             print("TRILOID")
-#         else:
-#             print(i)
+colors={'red','green','yellow'}
+for color in colors:
+    print(color)
+    for i in color:
+        if i=='r':
+            print("TRILOID")
+        else:
+            print(i)
 # for k in range(5):
 #     print(k+1)
 
 # for k in range(1,12,12):
 #     print(k)
-print("Now Abdulrehman is in main branch")
+# print("Now Abdulrehman is in main branch")
