3cd856d98766223e0f0b18c2c210d4bead0c9a2b
Your Name <rana.cht.45@gmail.com>
2025-10-17 02:45:31 -0700

    additional code in while loop

--- whileloop.py
+++ whileloop.py
@@ -1,5 +1,11 @@
-i=0
-while(i<=3):
+# i=0
+# while(i<=3):
+#     print(i)
+#     i=i+1
+# print("loop is end")
+i=30
+while(i<=38):
+    a=int(input("Enter Number:"))
+    i=i+a
     print(i)
-    i=i+1
-print("loop is end")
+print("Loop is end")
