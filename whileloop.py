951613933073776f78607ba5a74a19a1420b76dc
Your Name <rana.cht.45@gmail.com>
2025-10-17 02:55:25 -0700

    while loop updated

--- whileloop.py
+++ whileloop.py
@@ -3,9 +3,16 @@
 #     print(i)
 #     i=i+1
 # print("loop is end")
-i=30
-while(i<=38):
-    a=int(input("Enter Number:"))
-    i=i+a
-    print(i)
-print("Loop is end")
+# i=30
+# while(i<=38):
+#     a=int(input("Enter Number:"))
+#     i=i+a
+#     print(i)
+# print("Loop is end")
+a="abdulrehman"
+i=1
+while(i<=20):
+    print(i,a,"Loop is going")
+    i=i+1
+else:
+    print("Now loop is end,",a,"is in else")
